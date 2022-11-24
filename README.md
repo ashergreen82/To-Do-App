35f9452b3e85dc807423fcba6abe0e6f1aede91d
Ashergreen82 <greena@rogers.com>
2022-11-24 07:01:59 -0500

    Added the screenshot to the README file.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# To Do App
+# **A Simple To Do App**
+
+*A simple to do app written in Javascript, HTML, and CSS.*
 
 You can run the program from [here](https://ashergreen82.github.io/To-Do-App/).
 <br /><br />
 
 ![screenshot](/images/to%20do%20app%20screenshot.png)
+
+## **Summary**
+
+This Todo app has given me the practice in working with creating elements on the flyh with Javascript.  
+
+<br /><br />
 
 #### **Author**
 
--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function user_entered_something(e) {
 
     if (!userInput) return;
 
-    // Step is create a div which will be the outer container
+    // div container
     const container = document.createElement("div");
     container.setAttribute("class", "to_do_item");
     document.getElementById("todo_item_container").appendChild(container);
 
-    // This step is to create a delete button
+    // Delete button
     const delBtn = document.createElement("button");
     delBtn.innerText = "X";
     delBtn.setAttribute("class", "delete-button");
@@ -21,24 +21,22 @@ function user_entered_something(e) {
     container.appendChild(delBtn);
     delBtn.addEventListener("click", removeTodoItemfromList);
 
-    // This step is to create a label
+    // Label
     const label_1 = document.createElement("label");
     label_1.innerText = userInput;
     label_1.setAttribute("for", userInput);
     container.appendChild(label_1);
 
-    // This step is to create the checkbox
+    // Checkbox
     const checkBox = document.createElement("input");
     checkBox.setAttribute("name", userInput);
     checkBox.setAttribute("id", userInput);
     checkBox.setAttribute("class", "checkbox-button");
     checkBox.setAttribute("type", "checkbox");
     container.appendChild(checkBox);
-
-    // My event listener
     checkBox.addEventListener("change", user_checked_box);
 
-    // When the box is checked
+    // When the box is checked routine
     function user_checked_box(e) {
         isBoxChecked = e.target.checked;
         const deleteButton = e.target.previousSibling.previousSibling;
